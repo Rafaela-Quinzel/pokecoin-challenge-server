a78cddac9bf18d6fb85ac4eab4711edd9a1718aa
Your Name <you@example.com>
2022-08-22 15:38:05 -0300

    route adjustment

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const cors =  require('cors');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -16,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 mongoose.connect(DATABASE_URL);
