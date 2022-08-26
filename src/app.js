374a151929683b4634679228cc98df227ee40e86
Rafaela Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2022-08-26 15:55:02 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,9 +13,7 @@ const PORT = process.env.PORT || '27017';
 
 const app = express();
 
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
