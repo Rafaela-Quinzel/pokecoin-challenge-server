be69f2edcef4ab94d03fd8be7319e4ccf18a5704
Rafaela Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2022-09-01 23:13:05 -0300

    Update helper_jwt.js

--- src/helpers/helper_jwt.js
+++ src/helpers/helper_jwt.js
@@ -3,7 +3,7 @@ const dayjs = require('dayjs');
 const secret = require('../config/secret');
 
 async function generateToken(params = {}) {
-    const expiresIn = dayjs().add(15, 'seconds').unix();
+    const expiresIn = dayjs().add(dayjs.duration({'days' : 1})).unix();
 
     return jwt.sign(params, secret.aes, {
        // expiresIn: 86400,
